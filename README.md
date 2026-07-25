983db09912a6495bfbeda900158bd980a135616e
Shruthi Pragna Oruganti <182078145+shruthipragnao-2@users.noreply.github.com>
2026-07-25 19:40:20 +0530

    Initial commit of the completed project

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Redis_Clone
 
+A miniature Redis-protocol-compatible server in Python, built by following
+[Charles Leifer's tutorial](https://charlesleifer.com/blog/building-a-simple-redis-server-with-python/).
+It implements RESP (REdis Serialization Protocol) over a `gevent`-based TCP
+server, with an in-memory key/value store and six commands: `GET`, `SET`,
+`DELETE`, `FLUSH`, `MGET`, `MSET`.
+
+## Running
+
+```
+pip install -r requirements.txt
+python server.py
+```
+
+By default the server listens on `127.0.0.1:31337` (a non-standard port so it
+won't clash with a real Redis instance on 6379).
+
+## Talking to it
+
+With the real `redis-cli`:
+
+```
+redis-cli -p 31337
+127.0.0.1:31337> SET foo bar
+127.0.0.1:31337> GET foo
+127.0.0.1:31337> MSET a 1 b 2
+127.0.0.1:31337> MGET a b
+127.0.0.1:31337> DEL foo
+127.0.0.1:31337> FLUSHDB
+```
+
+Or with the bundled Python client:
+
+```
+python client.py
+```
+
---
+++ client.py
@@ -0,0 +1,47 @@
+import socket
+
+from server import Error, ProtocolHandler
+
+
+class Client(object):
+    def __init__(self, host='127.0.0.1', port=31337):
+        self._protocol = ProtocolHandler()
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._socket.connect((host, port))
+        self._fh = self._socket.makefile('rwb')
+
+    def execute(self, *args):
+        self._protocol.write_response(self._fh, list(args))
+        resp = self._protocol.handle_request(self._fh)
+        if isinstance(resp, Error):
+            raise Exception(resp.message)
+        return resp
+
+    def get(self, key):
+        return self.execute('GET', key)
+
+    def set(self, key, value):
+        return self.execute('SET', key, value)
+
+    def delete(self, key):
+        return self.execute('DELETE', key)
+
+    def flush(self):
+        return self.execute('FLUSH')
+
+    def mget(self, *keys):
+        return self.execute('MGET', *keys)
+
+    def mset(self, *items):
+        return self.execute('MSET', *items)
+
+
+if __name__ == '__main__':
+    client = Client()
+    client.flush()
+    client.set('foo', 'bar')
+    print('GET foo ->', client.get('foo'))
+    client.mset('a', '1', 'b', '2')
+    print('MGET a b ->', client.mget('a', 'b'))
+    print('DELETE foo ->', client.delete('foo'))
+    print('GET foo (after delete) ->', client.get('foo'))
